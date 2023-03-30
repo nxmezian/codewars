fb4af0960c5d9e44041d85d974e83e3bb7615dec
nxmezian <114146617+nxmezian@users.noreply.github.com>
2023-03-30 22:03:40 +0200

    Create messi.js

---
+++ messi.js
@@ -0,0 +1,7 @@
+#https://www.codewars.com/kata/55f73be6e12baaa5900000d4/train/javascript
+
+#calculates Messi's goals in La Liga, Copa del rey and Champions League
+
+function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+  return laLigaGoals + copaDelReyGoals + championsLeagueGoals
+}
