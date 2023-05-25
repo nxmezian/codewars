cdfa4ab53de1e355b8891b6eef53ab9f19d4d025
nxmezian <114146617+nxmezian@users.noreply.github.com>
2023-05-25 21:49:37 +0200

    Create alarm.js

---
+++ alarm.js
@@ -0,0 +1,10 @@
+#https://www.codewars.com/kata/568dcc3c7f12767a62000038/train/javascript
+function setAlarm(employed, vacation){
+  if (employed == true && vacation == false){
+    return true
+  } 
+  else {
+    return false
+  }
+
+}
